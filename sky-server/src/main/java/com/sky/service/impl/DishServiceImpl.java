43cac283e55a25511e2b339a0fadca26b41a0683
Yudie Zheng <y.zheng0031@gmail.com>
2024-04-29 12:32:54 +0200

    delete dishes optimized

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -59,11 +59,16 @@ public class DishServiceImpl implements DishService {
         }
 
         // 删除菜品表中的菜品数据
-        for (Long id : ids) {
-            dishMapper.deleteById(id);
-            // 删除菜品关联的口味数据
-            dishFlavorMapper.deleteByDishId(id);
-        }
+//        for (Long id : ids) {
+//            dishMapper.deleteById(id);
+//            // 删除菜品关联的口味数据
+//            dishFlavorMapper.deleteByDishId(id);
+//        }
+        // 根据菜品id集合批量删除菜品数据
+        dishMapper.deleteByIds(ids);
+
+        // 根据菜品id集合批量删除关联地口味数据
+        dishFlavorMapper.deleteByDishIds(ids);
 
     }
 
